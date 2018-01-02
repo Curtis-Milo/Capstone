5c5eea5a23e0a68eaf255a0c6e93b2aa954be611
Karim <kguirguis@yahoo.ca>
2018-01-01 22:25:02 -0500

    change method to need token

--- RasberryPi/lib/lib.py
+++ RasberryPi/lib/lib.py
@@ -2,11 +2,11 @@ import os
 import requests #set up requests on system
 
 # TODO: Set up env variables
-def sendErrorCode(code):
+def sendErrorCode(code, token):
 	host = 'http://' + os.environ['SERVER_IP'] + ':' + os.environ['SERVER_PORT'] + '/errors'
 
 	headers = {
-		"Authorization": "Bearer " + os.environ['TOKEN']
+		"Authorization": "Bearer " + token
 	}
 
 	return requests.post(host, headers=headers, data=code)
