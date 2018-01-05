ea4af1212707696fb3755f2bc653c8c9fb10ed57
Karim <kguirguis@yahoo.ca>
2018-01-04 19:10:22 -0500

    checking token in API for client actions

--- server/server.js
+++ server/server.js
@@ -4,11 +4,13 @@ const QUEUE = require('./queue/queue');
 const VERIF = require('./lib/verification');
 const SECURITY = require('./lib/security');
 const HELPER = require('./lib/helper');
+const TABLE_MANAGER = require('./lib/table_mgmt');
 
 var queue = new QUEUE();
 var order = new VERIF.Order();
 var tokenGen = new SECURITY.TokenGen();
 var authManager = new SECURITY.BasicAuthManager();
+var tableManager = new TABLE_MANAGER.TableManager();
 
 tokenGen.sendToken(process.env.BOT_HOST); // Define bot IP in environment
 
@@ -16,16 +18,38 @@ HTTP.createServer(function(req, res) {
 	var url = URL.parse(req.url, true);
 	if (req.method.toUpperCase() === 'GET') {
 		if (url.pathname.toLowerCase().replace(/\//, '') === 'placeinline') {
-			var order_id = HELPER.caseInsensitiveKey(url.query, 'order_id');
-			queue.search(order_id, function(err, placeInLine) {
-				if (err) {
-					res.writeHead(404, err, {'Content-Type': 'text/html'});
+			var auth = HELPER.caseInsensitiveKey(req.headers, 'authorization');
+			var table_id = HELPER.caseInsensitiveKey(url.query, 'table_id');
+
+			if (! auth || ! table_id) {
+				res.writeHead(401, 'Unauthorized', {'Content-Type': 'text/html'});
+				res.end();
+				return;
+			}
+
+			var token = auth.trim().split(' ')[1];
+
+			tableManager.checkToken(table_id, token, function(authErr, passed) {
+				if (authErr) {
+					res.writeHead(500, authErr, {'Content-Type': 'text/html'});
 					res.end();
-				} else {
-					res.writeHead(200, {'Content-Type': 'text/html'});
-					res.write(placeInLine.toString());
+					return;
+				} else if (! passed) {
+					res.writeHead(401, 'Unauthorized', {'Content-Type': 'text/html'});
 					res.end();
+					return;
 				}
+				var order_id = HELPER.caseInsensitiveKey(url.query, 'order_id');
+				queue.search(order_id, function(err, placeInLine) {
+					if (err) {
+						res.writeHead(404, err, {'Content-Type': 'text/html'});
+						res.end();
+					} else {
+						res.writeHead(200, {'Content-Type': 'text/html'});
+						res.write(placeInLine.toString());
+						res.end();
+					}
+				});
 			});
 		} else if (url.pathname.toLowerCase().replace(/\//, '') === 'nextorder') {
 			var auth = HELPER.caseInsensitiveKey(req.headers, 'authorization');
@@ -104,39 +128,61 @@ HTTP.createServer(function(req, res) {
 		}
 	} else if (req.method.toUpperCase() === 'POST') {
 		if (url.pathname.toLowerCase().replace(/\//, '') === 'placeorder') {
-			var body = '';
-			req.on('data', function(data) {
-				body += data;
-			});
+			var auth = HELPER.caseInsensitiveKey(req.headers, 'authorization');
+			var table_id = HELPER.caseInsensitiveKey(url.query, 'table_id');
 
-			req.on('end', function() {
-				try {
-					body = JSON.parse(body);
-				} catch (e) {
-					res.writeHead(500, 'Invalid body', {'Content-Type': 'text/html'});
+			if (! auth || ! table_id) {
+				res.writeHead(401, 'Unauthorized', {'Content-Type': 'text/html'});
+				res.end();
+				return;
+			}
+
+			var token = auth.trim().split(' ')[1];
+
+			tableManager.checkToken(table_id, token, function(authErr, passed) {
+				if (authErr) {
+					res.writeHead(500, authErr, {'Content-Type': 'text/html'});
+					res.end();
+					return;
+				} else if (! passed) {
+					res.writeHead(401, 'Unauthorized', {'Content-Type': 'text/html'});
 					res.end();
 					return;
 				}
+				var body = '';
+				req.on('data', function(data) {
+					body += data;
+				});
 
-				VERIF.checkData(body, order, function(err, newData) {
-					if (err) {
-						res.writeHead(500, err, {'Content-Type': 'text/html'});
+				req.on('end', function() {
+					try {
+						body = JSON.parse(body);
+					} catch (e) {
+						res.writeHead(500, 'Invalid body', {'Content-Type': 'text/html'});
 						res.end();
 						return;
 					}
 
-					queue.push(newData, function(queueErr, placeInLine) {
-						if (queueErr) {
-							res.writeHead(500, queueErr, {'Content-Type': 'text/html'});
+					VERIF.checkData(body, order, function(err, newData) {
+						if (err) {
+							res.writeHead(500, err, {'Content-Type': 'text/html'});
 							res.end();
 							return;
 						}
-						res.writeHead(200, {'Content-Type': 'application/json'});
-						res.write(JSON.stringify({
-							placeInLine: placeInLine,
-							orderData: newData
-						}));
-						res.end();
+
+						queue.push(newData, function(queueErr, placeInLine) {
+							if (queueErr) {
+								res.writeHead(500, queueErr, {'Content-Type': 'text/html'});
+								res.end();
+								return;
+							}
+							res.writeHead(200, {'Content-Type': 'application/json'});
+							res.write(JSON.stringify({
+								placeInLine: placeInLine,
+								orderData: newData
+							}));
+							res.end();
+						});
 					});
 				});
 			});
@@ -317,15 +363,37 @@ HTTP.createServer(function(req, res) {
 		}
 	} else if (req.method.toUpperCase() === 'DELETE') {
 		if (url.pathname.toLowerCase().replace(/\//, '') === 'cancelorder') {
-			var order_id = HELPER.caseInsensitiveKey(url.query, 'order_id');
-			queue.delete(order_id, function(err) {
-				if (err) {
-					res.writeHead(500, err, {'Content-Type': 'text/html'});
+			var auth = HELPER.caseInsensitiveKey(req.headers, 'authorization');
+			var table_id = HELPER.caseInsensitiveKey(url.query, 'table_id');
+
+			if (! auth || ! table_id) {
+				res.writeHead(401, 'Unauthorized', {'Content-Type': 'text/html'});
+				res.end();
+				return;
+			}
+
+			var token = auth.trim().split(' ')[1];
+
+			tableManager.checkToken(table_id, token, function(authErr, passed) {
+				if (authErr) {
+					res.writeHead(500, authErr, {'Content-Type': 'text/html'});
 					res.end();
-				} else {
-					res.writeHead(200);
+					return;
+				} else if (! passed) {
+					res.writeHead(401, 'Unauthorized', {'Content-Type': 'text/html'});
 					res.end();
+					return;
 				}
+				var order_id = HELPER.caseInsensitiveKey(url.query, 'order_id');
+				queue.delete(order_id, function(err) {
+					if (err) {
+						res.writeHead(500, err, {'Content-Type': 'text/html'});
+						res.end();
+					} else {
+						res.writeHead(200);
+						res.end();
+					}
+				});
 			});
 		} else {
 			res.writeHead(404, 'No such method', {'Content-Type': 'text/html'});
