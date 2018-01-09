e91430cffba869d8ad9515fd3c2a51c2d180caf5
Karim <kguirguis@yahoo.ca>
2018-01-09 15:39:18 -0500

    fix constructor

--- server/server.js
+++ server/server.js
@@ -17,7 +17,7 @@ var queue = new QUEUE();
 var order = new VERIF.Order();
 var tokenGen = new SECURITY.TokenGen();
 var authManager = new SECURITY.BasicAuthManager();
-var tableManager = new TABLE_MANAGER.TableManager();
+var tableManager = new TableManager();
 
 const MAX_NUM_TYPES = 3;
 
