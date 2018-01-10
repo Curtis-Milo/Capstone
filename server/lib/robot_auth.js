c594d126e7473772d0f088abf9de6a34d4ef5ed9
Karim <kguirguis@yahoo.ca>
2018-01-10 16:00:27 -0500

    :// after http

--- server/lib/robot_auth.js
+++ server/lib/robot_auth.js
@@ -37,8 +37,8 @@ TokenGen.prototype.checkToken = function(token, cb) {
 
 //send generated token to robot
 TokenGen.prototype.sendToken = function(host) {
-	if (! host.startsWith('http')) {
-		host = 'http' + host;
+	if (! host.startsWith('http://')) {
+		host = 'http://' + host;
 	}
 	var data = {
 		token_type: 'bearer',
