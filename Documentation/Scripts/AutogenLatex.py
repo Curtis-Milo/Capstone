f83302ba987c3889e9fbf86f4933ff59fa8f12c3
Curtis Milo <curtmiloz@gmail.com>
2018-02-08 13:59:27 -0500

    adding unit test example

--- Documentation/Scripts/AutogenLatex.py
+++ Documentation/Scripts/AutogenLatex.py
@@ -1,10 +1,10 @@
 inF = file("in.txt","w")
-outData= "\begin{table}[]\\n \
+outData= "\\begin{table}[]\\n \
     \\centering\\n \
     \\caption{My caption}\\n \
-    \\begin{tabular}{1lllll}\\n"
+    \\begin{tabular}{11lllll}\\n"
 lines = inF.readlines()
-tableRow = "Test Number & Discription & Inputs & Expected Outputs \
+tableRow = "Test Number & Discription & Requirement Reference & Inputs & Expected Outputs \
  & Actual Outputs & Results \\\\\\n"
 
 outData= outData+tableRow
@@ -12,11 +12,12 @@ for i in range(len(lines)):
     data = lines[i].split(",")
     testNum =data[0]
     testDis = data[1]
-    intputs = data[2]
-    expOutputs = data[3]
-    actOutputs = data[4]
-    res= data[5]
-    tableRow = testNum +" & "+testDis+" & "+intputs+\
+    reqDis = data[2]
+    intputs = data[3]
+    expOutputs = data[4]
+    actOutputs = data[5]
+    res= data[6]
+    tableRow = testNum +" & "+testDis+" & "+reqDis+" & "+intputs+\
                " & "+ expOutputs+" & "+actOutputs+" & "+ res +" \\\\\\n"
     outData= outData+tableRow
     
