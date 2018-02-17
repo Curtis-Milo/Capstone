84a15d75092e4dced73e3255573103725184026c
MARKO LABAN <markosameh@gmail.com>
2018-02-17 17:07:35 -0500

    bug fixes

--- RasberryPi/pathFindingTest.py
+++ RasberryPi/pathFindingTest.py
@@ -10,22 +10,23 @@ class pathFindingTest:
 	#used to determine the previous line size
 		pos = 0
 		lines = information.split('\n')
-	#Used so we done inflate memory like crazy with replicas of the same object
+	#Used so we dont inflate memory like crazy with replicas of the same object
 		height = len(lines)
 		for i in range(len(lines)):
+			
 			chars = lines[i].split(',')
 			if (i == 0):
 				width = len(chars)
-
 			for j in range(len(chars)):
-				self.map.add_node((j,i))
+				self.map.add_node((i,j))
 
 		for i in range(len(lines)):	
 			chars = lines[i].strip().split(',')
 			
-			for j in range(len(chars)):
+			for j in range(len(chars)-1):
+				
 				cost = 1
-				cur_Node = (j,i)
+				cur_Node = (i,j)
 				#print chars[j] + str(cur_Node)
 				#determining the starting and ending position
 				if (chars[j] == "H"):
@@ -33,59 +34,80 @@ class pathFindingTest:
 					source = (j,i)
 					home_num = home_num +1
 				elif (chars[j] == "T"):
+					#print cur_Node
 					self.tablesList.append(cur_Node)
 					table_num = table_num+1
 
 				elif (chars[j] == "X"):
 					continue
+				
 			
 
 				#creating the edges and adding them to the graphs
 				
+##				print chars
 				if (0 < j): 
-					prev = (j-1, i)
-					passable = self.determinePassable(lines,prev,cur_Node)
-					if passable:
-						self.map.add_edge(cur_Node, prev, 1)
-
-				if (j < len(chars) - 1):
-					nextN =(j + 1, i)
-					passable = self.determinePassable(lines,cur_Node,nextN)
-					if passable:
+					prev = (i,j)
+					table,passable = self.determinePassable(lines,prev,cur_Node)
+					if passable and not table:
+						self.map.add_edge(prev,cur_Node, 1)
+					elif table and not passable:
+						self.map.add_edge(prev, cur_Node,float("inf"))
+						
+				if (j < len(chars)):
+					nextN =(i,j+1)
+					table, passable = self.determinePassable(lines,cur_Node,nextN)
+					if passable and not table:
 						self.map.add_edge(cur_Node, nextN, 1)
+					elif table and not passable:
+						self.map.add_edge(cur_Node, nextN, float("inf"))
 
 				if (0 < i):
-					prev = (j, i-1)
-					passable = self.determinePassable(lines,prev,cur_Node)
-					if passable:
-						self.map.add_edge(cur_Node, prev, 1)
-					
-				if (i < len(lines) - 1):
-					nextN =(j + 1, i)
-					passable = self.determinePassable(lines,cur_Node,nextN)
-					if passable:
+					prev = (i-1,j)
+					table, passable = self.determinePassable(lines,prev,cur_Node)
+					if passable and not table:
+						self.map.add_edge(prev, cur_Node, 1)
+					elif table and not passable:
+						self.map.add_edge(prev, cur_Node, float("inf"))
+						
+				if (i < len(lines)):
+					nextN =(i,j+1)
+					table, passable = self.determinePassable(lines,cur_Node,nextN)
+					if passable and not table:
 						self.map.add_edge(cur_Node, nextN, 1)
+					elif table and  not passable:
+						self.map.add_edge(cur_Node, nextN, float("inf"))
+
 
 	def determinePassable(self, lines, node1, node2):
-		node1Char = lines[node1[1]].strip().split(',')[node1[0]].strip()
-		node2Char = lines[node2[1]].strip().split(',')[node2[0]].strip()
+		
+		node1Char = lines[node1[0]].strip().split(',')[node1[1]].strip()
+		node2Char = lines[node2[0]].strip().split(',')[node2[1]].strip()
 
+		
 		isT = node1Char == 'T' or node2Char == 'T'
 		isX = node1Char == 'X' or node2Char == 'X'
 
-		if isT or isX:
-			return False
+		if isX:
+			return False, False
+		elif isT:
+			return True, False
 		else:
-			return True
+			return False,True
+
+       
 
 p =pathFindingTest()
 currNode = (0,0)
 visited, path = p.map.dijsktra(currNode)
 node = p.tablesList[0]
-nodesToTravel = []
 
-while (node != currNode):
-	nodesToTravel.insert(0,path[node]);
-	node= path[node]
 
-print nodesToTravel
+for table in p.tablesList:
+        nodesToTravel = []
+        node = table
+        while (node != currNode):
+                nodesToTravel.insert(0,path[node]);
+                node= path[node]
+
+        print table, nodesToTravel
