11abc9395dd5b0da94de9128ed38b9940531db08
Karim <kguirguis@yahoo.ca>
2018-02-17 13:09:16 -0500

    add tests and promisify tests

--- server/test/server.js
+++ server/test/server.js
@@ -24,14 +24,32 @@ const IP = 'http://localhost:8080';
 var tests = {
 	generalTest: {
 		getDrinks: function(resObj, host) {
-			unirest.get(host + '/drinks')
-			.headers({'Accept': 'application/json', 'Content-Type': 'application/json'})
-			.end(function(res) {
-				if (res.code < 200 || res.code > 299) {
-					resObj.testRes('Test GET /drinks endpoint', 'F', 200, res.code, 'fail');
-				} else {
-					resObj.testRes('Test GET /drinks endpoint', 'F', 200, res.code, 'pass');
-				}
+			return new Promise(function(resolve, reject) {
+				unirest.get(host + '/drinks')
+				.headers({'Accept': 'application/json', 'Content-Type': 'application/json'})
+				.end(function(res) {
+					if (res.code < 200 || res.code > 299) {
+						resObj.testRes('Test GET /drinks endpoint', 'F', 200, res.code, 'fail');
+					} else {
+						resObj.testRes('Test GET /drinks endpoint', 'F', 200, res.code, 'pass');
+					}
+					resolve();
+				});
+			});
+		},
+
+		getNumOfTanks: function(resObj, host) {
+			return new Promise(function(resolve, reject) {
+				unirest.get(host + '/numOfTanks')
+				.headers({'Accept': 'application/json', 'Content-Type': 'application/json'})
+				.end(function(res) {
+					if (res.code < 200 || res.code > 299) {
+						resObj.testRes('Test GET /numOfTanks endpoint', 'F', 200, res.code, 'fail');
+					} else {
+						resObj.testRes('Test GET /numOfTanks endpoint', 'F', 200, res.code, 'pass');
+					}
+					resolve();
+				});
 			});
 		}
 	},
@@ -43,45 +61,68 @@ var tests = {
 		},
 
 		updateCreds: function(resObj, host) {
-			var data = {
-				userName: 'admin_new',
-				password: 'admin_new'
-			};
+			return new Promise(function(resolve, reject) {
+				var data = {
+					userName: 'admin_new',
+					password: 'admin_new'
+				};
 
-			unirest.post(host + '/updateCreds')
-			.headers({'Accept': 'application/json', 'Content-Type': 'application/json'})
-			.auth(this._creds.userName, this._creds.password)
-			.send(data)
-			.end(function (res) {
-				if (res.code < 200 || res.code > 299) {
-					resObj.testRes('Test POST /updateCreds endpoint', 'F', 200, res.code, 'fail');
-				} else {
-					unirest.post(host + '/updateCreds')
-					.headers({'Accept': 'application/json', 'Content-Type': 'application/json'})
-					.auth(data.userName, data.password)
-					.send(this._creds)
-					.end(function(res) {
-						if (res.code < 200 || res.code > 299) {
-							resObj.testRes('Test POST /updateCreds endpoint', 'F', 200, res.code, 'fail');
-						} else {
-							resObj.testRes('Test POST /updateCreds endpoint', 'F', 200, res.code, 'pass');
-						}
-					});
-				}
+				unirest.post(host + '/updateCreds')
+				.headers({'Accept': 'application/json', 'Content-Type': 'application/json'})
+				.auth(this._creds.userName, this._creds.password)
+				.send(data)
+				.end(function (res) {
+					if (res.code < 200 || res.code > 299) {
+						resObj.testRes('Test POST /updateCreds endpoint', 'F', 200, res.code, 'fail');
+					} else {
+						unirest.post(host + '/updateCreds')
+						.headers({'Accept': 'application/json', 'Content-Type': 'application/json'})
+						.auth(data.userName, data.password)
+						.send(this._creds)
+						.end(function(res) {
+							if (res.code < 200 || res.code > 299) {
+								resObj.testRes('Test POST /updateCreds endpoint', 'F', 200, res.code, 'fail');
+							} else {
+								resObj.testRes('Test POST /updateCreds endpoint', 'F', 200, res.code, 'pass');
+							}
+						});
+					}
+					resolve();
+				});
 			});
 		},
 
 		login: function(resObj, host) {
-			unirest.post(host + '/login')
-			.headers({'Accept': 'application/json', 'Content-Type': 'application/json'})
-			.auth(this._creds.userName, this._creds.password)
-			.end(function(res) {
-				if (res.code < 200 || res.code > 299) {
-					resObj.testRes('Test POST /login endpoint', 'F', 200, res.code, 'fail');
-				} else {
-					resObj.testRes('Test POST /login endpoint', 'F', 200, res.code, 'pass');
-				}
+			return new Promise(function(resolve, reject) {
+				unirest.post(host + '/login')
+				.headers({'Accept': 'application/json', 'Content-Type': 'application/json'})
+				.auth(this._creds.userName, this._creds.password)
+				.end(function(res) {
+					if (res.code < 200 || res.code > 299) {
+						resObj.testRes('Test POST /login endpoint', 'F', 200, res.code, 'fail');
+					} else {
+						resObj.testRes('Test POST /login endpoint', 'F', 200, res.code, 'pass');
+					}
+					resolve();
+				});
 			});
 		},
+
+		setMap: function(resObj, host) {
+			return new Promise(function(resolve, reject) {
+				unirest.post(host + '/map')
+				.headers({'Accept': 'application/json', 'Content-Type': 'application/json'})
+				.auth(this._creds.userName, this._creds.password)
+				.attach('file', './map_test.txt')
+				.end(function(res) {
+					if (res.code < 200 || res.code > 299) {
+						resObj.testRes('Test POST /map endpoint', 'F', 200, res.code, 'fail');
+					} else {
+						resObj.testRes('Test POST /map endpoint', 'F', 200, res.code, 'pass');
+					}
+					resolve();
+				});
+			});
+		}
 	}
 }
