4d52aef5083281d96a1ebac3b8fb54965daf635b
Karim <kguirguis@yahoo.ca>
2018-02-19 00:36:56 -0500

    try anything

--- server/test/server.js
+++ server/test/server.js
@@ -139,28 +139,29 @@ var tests = {
 		setMap: function(resObj, host) {
 			var that = this;
 			return new Promise(function(resolve, reject) {
-				fs.createReadStream('./map_test.txt').pipe(request.post(host + '/map')
-					.on('response', function(res) {
-						if (res.statusCode < 200 || res.statusCode > 299) {
-							resObj.testRes('Test POST /map endpoint', 'AD1,AD2', 200, res.statusCode, 'fail');
-						} else {
-							resObj.testRes('Test POST /map endpoint', 'AD1,AD2', 200, res.statusCode, 'pass');
-						}
-						resolve();
-					}));
-				// unirest.post(host + '/map')
-				// .headers({'Accept': 'application/json'})
-				// .auth(that._creds.userName, that._creds.password)
-				// .attach({'relative file': fs.createReadStream('./map_test.txt')})
-				// .stream()
-				// .end(function(res) {
-				// 	if (res.code < 200 || res.code > 299) {
-				// 		resObj.testRes('Test POST /map endpoint', 'AD1,AD2', 200, res.code, 'fail');
-				// 	} else {
-				// 		resObj.testRes('Test POST /map endpoint', 'AD1,AD2', 200, res.code, 'pass');
-				// 	}
-				// 	resolve();
-				// });
+				// fs.createReadStream('./map_test.txt').pipe(request.post(host + '/map')
+				// 	.on('response', function(res) {
+				// 		if (res.statusCode < 200 || res.statusCode > 299) {
+				// 			resObj.testRes('Test POST /map endpoint', 'AD1,AD2', 200, res.statusCode, 'fail');
+				// 		} else {
+				// 			resObj.testRes('Test POST /map endpoint', 'AD1,AD2', 200, res.statusCode, 'pass');
+				// 		}
+				// 		resolve();
+				// 	}));
+
+				unirest.post(host + '/map')
+				.headers({'Accept': 'application/json'})
+				.auth(that._creds.userName, that._creds.password)
+				.stream()
+				.attach('file', './map_test.txt')
+				.end(function(res) {
+					if (res.code < 200 || res.code > 299) {
+						resObj.testRes('Test POST /map endpoint', 'AD1,AD2', 200, res.code, 'fail');
+					} else {
+						resObj.testRes('Test POST /map endpoint', 'AD1,AD2', 200, res.code, 'pass');
+					}
+					resolve();
+				});
 			});
 		},
 
