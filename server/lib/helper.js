641f3d4dae8478810a8078675741093c6ecbd403
Karim <kguirguis@yahoo.ca>
2018-03-08 10:37:47 -0500

    flip order of sort

--- server/lib/helper.js
+++ server/lib/helper.js
@@ -1,9 +1,9 @@
 function _getSortOrder(prop) {
 	return function(a, b) {
 		if (a[prop] > b[prop]) {
-			return 1;  
+			return -1;  
 		} else if (a[prop] < b[prop]) {
-			return -1;
+			return 1;
 		}
 		return 0;
 	}
