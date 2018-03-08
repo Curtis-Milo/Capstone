6133c7fe1dfc37062dc1b784ed6b8febb75e5437
Karim <kguirguis@yahoo.ca>
2018-03-08 00:00:39 -0500

    unecessary parsing

--- server/lib/helper.js
+++ server/lib/helper.js
@@ -22,15 +22,13 @@ module.exports = {
 		var lower = 0;
 		var upper = list.length - 1;
 
-		element = parseInt(element);
-
 		while (lower <= upper) {
 			var mid = Math.floor((upper + lower) / 2);
 			var curr = list[mid];
 
-			if (parseInt(curr[attr]) > element) {
+			if (curr[attr] > element) {
 				lower = mid + 1;
-			} else if (parseInt(curr[attr]) < element) {
+			} else if (curr[attr] < element) {
 				upper = mid - 1;
 			} else {
 				return mid;
