757a591d0d822565810da09f12c73b6d6007a76f
keypat <keyur.np08@gmail.com>
2018-03-16 22:40:45 -0400

    progrss

--- ManagerSystem/startbootstrap-sb-admin-gh-pages/js/helper.js
+++ ManagerSystem/startbootstrap-sb-admin-gh-pages/js/helper.js
@@ -23,7 +23,7 @@ window.onload = function() {
     
     //dev mode
     if (document.location.hostname=='localhost' && document.location.port==3000) {
-        if (path=='/' || path=='/index.html') {
+        if (path=='/' || path=='/errors.html') {
             if (document.location.hostname=='localhost' && document.location.port==3000) {
                 //testing at home
                 setErrorList(15);
@@ -34,7 +34,7 @@ window.onload = function() {
         }
     //prod
     } else {
-        if (path=='/' || path=='/index.html') {
+        if (path=='/' || path=='/errors.html') {
             NetworkCall('getErrors');
         } else if (path=='/charts.html') {
             parseFiles();
@@ -237,7 +237,6 @@ function navigate(dest) {
 }
 
 function NetworkCall(api_key, objects) {
-    var xhttp = new XMLHttpRequest();
     if (api_key=='login') {
         var xhttp = createCORSRequest('POST', "/proxy/login");
         if (!xhttp) {
@@ -245,37 +244,23 @@ function NetworkCall(api_key, objects) {
         }
         var temp = String(objects[0]) + ":" + String(objects[1]);
         var basic = btoa(temp);
-        // xhttp.open("POST", "http://130.113.68.87:8080/login");
-        xhttp.setRequestHeader('Authorization', "Basic " + basic);
-        // xhttp.setRequestHeader("Access-Control-Allow-Origin", "*");
-        // xhttp.setRequestHeader("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT, OPTIONS");
 
-        var tempResp;
-        xhttp.onload = function() {
-            tempResp = xhttp.responseText;
-        }
-        xhttp.onerror = function() {
-            console.log("error");
-        }
+        xhttp.setRequestHeader('Authorization', "Basic " + basic);
+
         xhttp.onreadystatechange = function() {
             if (xhttp.status == 200) {
-                navigate("../index.html");
+                navigate("../errors.html");
             } else {
                 console.log("Error: " + xhttp.responseText);
             }
         }
         xhttp.send();
+
+
     } else if (api_key=='getErrors') {
         var xhttp = createCORSRequest('GET','/proxy/errors');
         if (!xhttp) {
             throw new Error('CORS not supported');
-        }
-        var tempResp;
-        xhttp.onload = function() {
-            tempResp = xhttp.responseText;
-        }
-        xhttp.onerror = function() {
-            console.log("error");
         }
         xhttp.onreadystatechange = function() {
             if (xhttp.status == 200) {
@@ -285,17 +270,13 @@ function NetworkCall(api_key, objects) {
             }
         }
         xhttp.send();
+
+
+
     } else if (api_key=='save_map') {
         var xhttp = createCORSRequest('POST','/proxy/map');
         if (!xhttp) {
             throw new Error('CORS not supported');
-        }
-        var tempResp;
-        xhttp.onload = function() {
-            tempResp = xhttp.responseText;
-        }
-        xhttp.onerror = function() {
-            console.log("error");
         }
         xhttp.onreadystatechange = function() {
             if (xhttp.status == 200) {
@@ -306,18 +287,14 @@ function NetworkCall(api_key, objects) {
             }
         }   
         xhttp.send(mapFile);
+
+
     } else if (api_key=='load_map') {
         var xhttp = createCORSRequest('GET','/proxy/map');
         if (!xhttp) {
             throw new Error('CORS not supported');
         }
-        var tempResp;
-        xhttp.onload = function() {
-            tempResp = xhttp.responseText;
-        }
-        xhttp.onerror = function() {
-            console.log("error");
-        }
+        xhttp.send();
         xhttp.onreadystatechange = function() {
             if (xhttp.status == 200) {
                 this.mapFile = String(xhttp.responseText);
@@ -328,6 +305,5 @@ function NetworkCall(api_key, objects) {
                 console.log("Error: " + xhttp.responseText);
             }
         }   
-        xhttp.send();
     }
 }
