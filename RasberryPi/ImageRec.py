455dd9c8b77e8509927b0e33322b89f37db01f9d
Curtmiloz <Curtmiloz@gmail.com>
2018-03-17 22:56:29 -0400

    added new version of image rec

--- RasberryPi/ImageRec.py
+++ RasberryPi/ImageRec.py
@@ -2,42 +2,69 @@ import numpy as np
 import argparse
 import cv2
 import math
-import picamera
+#import picamera
 #https://www.raspberrypi.org/documentation/usage/camera/python/README.md
 #https://www.pyimagesearch.com/2014/07/21/detecting-circles-images-using-opencv-hough-circles/
-Global_Camera = picamera.PiCamera()
+#Global_Camera = picamera.PiCamera()
 class ImageRec():
-    
-    def __init__(self):
-        #CALS
-        self.mid_x = 1920.0/2.0
-        self.mid_y = 1080.0/2.0
-        self.end_x = 1920*(0.8)
-        self.end_y = 1080*(0.8)
-        self.before_x = 1920*(0.2)
-        self.before_y = 1080*(0.2)
-        self.hist = 100.0
+	
+	def __init__(self):
+		#CALS
+		self.mid_x = 1920.0/2.0
+		self.mid_y = 1080.0/2.0
+		self.end_x = 1920*(0.8)
+		self.end_y = 1080*(0.8)
+		self.before_x = 1920*(0.2)
+		self.before_y = 1080*(0.2)
+		self.hist = 100.0
 
-    def captureImage(self):
-        Global_Camera.capture('ceiling.jpg')
+	def captureImage(self):
+		Global_Camera.capture('ceiling10.jpg')
 
-    def checkForCircle(self):
-        img = cv2.imread('ceiling.jpg',0)
-        # detect circles in the image
-        circles = cv2.HoughCircles(img, cv2.cv.CV_HOUGH_GRADIENT, 1.2, 100)
-        return circles
+	def checkForCircle(self):
 
-    def test(self):
+		original = cv2.imread('ceiling10.jpg', cv2.CV_LOAD_IMAGE_GRAYSCALE)
+		retval, image = cv2.threshold(original, 50, 255, cv2.cv.CV_THRESH_BINARY)
+
+		el = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (5, 5))
+		image = cv2.dilate(image, el, iterations=6)
+
+		cv2.imwrite("dilated.png", image)
+
+		contours, hierarchy = cv2.findContours(
+			image,
+			cv2.cv.CV_RETR_LIST,
+			cv2.cv.CV_CHAIN_APPROX_SIMPLE
+		)
+
+		drawing = cv2.imread('ceiling10.jpg')
+
+		circles = []
+		for contour in contours:
+			area = cv2.contourArea(contour)
+
+			# there is one contour that contains all others, filter it out
+			if area > 500:
+				continue
+
+			br = cv2.boundingRect(contour)
+
+			m = cv2.moments(contour)
+			center = [int(m['m10'] / m['m00']), int(m['m01'] / m['m00']),br[2]]
+			circles.append(center)
+
+		return circles
+
+	def test(self):
 		print "Capturing Image... "
-		i = ImageRec()
-		i.captureImage()
-		circles = i.checkForCircle()
+		#i = ImageRec()
+		#i.captureImage()
+		circles = self.checkForCircle()
 
 		if circles is not None:
-		    # convert the (x, y) coordinates and radius of the circles to integers
-		    circles = np.round(circles[0, :]).astype("int")
-		             
-		    # loop over the (x, y) coordinates and radius of the circles
-		    for (x, y, r) in circles:
-		        print "Circle At: " + str(x) +", "+ str(y)
-		        print "Radius of: " + str(r)
+			# convert the (x, y) coordinates and radius of the circles to integers
+	 
+			# loop over the (x, y) coordinates and radius of the circles
+			for (x, y, r) in circles:
+				print "Circle At: " + str(x) +", "+ str(y)
+				print "Radius of: " + str(r)
